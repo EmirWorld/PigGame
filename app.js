3b7a048592a855d778dd942696322be7fd642337
Emir Kovacevic <emirkovacevic@protonmail.com>
2021-01-12 17:13:02 +0100

    Implementing logic for game

--- app.js
+++ app.js
@@ -30,6 +30,13 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
   document.querySelector('#current-' + activePlayer).textContent = dice;
 
   //Update round result if its not rolled 1
-
+  if (dice !== 1){
+    //add score
+    roundScore += dice;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
+  }else{
+    // Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer =0;
+  }
 
 });
